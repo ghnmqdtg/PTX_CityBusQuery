a13cd1f404bece95f51452d9bbbd932f304b6538
ghnmqdtg <ghnmqdtg@gmail.com>
2020-06-29 12:59:57 +0800

    Extract the authorization process as an independent module

--- Parse_Routes_info.py
+++ Parse_Routes_info.py
@@ -1,13 +1,8 @@
 # -*- coding: UTF-8 -*-
-from hashlib import sha1
-import hmac
-from wsgiref.handlers import format_date_time
-from datetime import datetime
-from time import mktime
-import base64
 import requests
 import json
 import configparser
+import Authorization
 
 config = configparser.ConfigParser()
 config.read("config.ini")
@@ -16,33 +11,12 @@ APP_ID = config["AUTH"]["APP_ID"]
 APP_KEY = config["AUTH"]["APP_KEY"]
 RESOURCE_PATH = config["API"]["Route_TNN"]
 
-class Auth():
-
-    def __init__(self, APP_ID, APP_KEY):
-        self.APP_ID = APP_ID
-        self.APP_KEY = APP_KEY
-
-    def get_auth_header(self):
-        xdate = format_date_time(mktime(datetime.now().timetuple()))
-        hashed = hmac.new(self.APP_KEY.encode('utf8'), ('x-date: ' + xdate).encode('utf8'), sha1)
-        signature = base64.b64encode(hashed.digest()).decode()
-
-        authorization = 'hmac username="' + self.APP_ID + '", ' + \
-                        'algorithm="hmac-sha1", ' + \
-                        'headers="x-date", ' + \
-                        'signature="' + signature + '"'
-        return {
-            'Authorization': authorization,
-            'x-date': format_date_time(mktime(datetime.now().timetuple())),
-            'Accept - Encoding': 'gzip'
-        }
-
 
 def Parse_Routes_info():
     # datatype : dict
-    response = requests.get(RESOURCE_PATH, headers= auth.get_auth_header()).json()
-    raw = response["Routes"]
-    for x in raw:
+    raw = requests.get(RESOURCE_PATH, headers=auth.get_auth_header()).json()
+    data = raw["Routes"]
+    for x in data:
         dict_routes[x["RouteName"]["Zh_tw"]] = x["RouteUID"]
     # print(json.dumps(dict_routes, indent=4, ensure_ascii=False))
 
@@ -50,7 +24,7 @@ def Parse_Routes_info():
 if __name__ == '__main__':
     count = 0
     dict_routes = {}
-    auth = Auth(APP_ID, APP_KEY)
+    auth = Authorization.Auth(APP_ID, APP_KEY)
     Parse_Routes_info()
 
     # Set the path and filename to save routeUID dictionary
--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 # -*- coding: UTF-8 -*-
-from hashlib import sha1
-import hmac
-from wsgiref.handlers import format_date_time
-from datetime import datetime
-from time import mktime
-import base64
 import requests
 import json
 import configparser
 from urllib import parse
+import Authorization
 
 config = configparser.ConfigParser()
 config.read("config.ini")
@@ -16,27 +11,6 @@ config.read("config.ini")
 APP_ID = config["AUTH"]["APP_ID"]
 APP_KEY = config["AUTH"]["APP_KEY"]
 RESOURCE = config["API"]["Estimated_TNN"]
-
-class Auth():
-
-    def __init__(self, APP_ID, APP_KEY):
-        self.APP_ID = APP_ID
-        self.APP_KEY = APP_KEY
-
-    def get_auth_header(self):
-        xdate = format_date_time(mktime(datetime.now().timetuple()))
-        hashed = hmac.new(self.APP_KEY.encode('utf8'), ('x-date: ' + xdate).encode('utf8'), sha1)
-        signature = base64.b64encode(hashed.digest()).decode()
-
-        authorization = 'hmac username="' + self.APP_ID + '", ' + \
-                        'algorithm="hmac-sha1", ' + \
-                        'headers="x-date", ' + \
-                        'signature="' + signature + '"'
-        return {
-            'Authorization': authorization,
-            'x-date': format_date_time(mktime(datetime.now().timetuple())),
-            'Accept - Encoding': 'gzip'
-        }
 
 
 class Bus:
@@ -55,24 +29,27 @@ class Bus:
         count = 0
         # urllib.parse.quote() URL encode
         QUERY_OPTIONS = parse.quote(self.routename) + "?$format=JSON"
-        response = requests.get(RESOURCE + QUERY_OPTIONS, headers= auth.get_auth_header()).json()
-        # print(json.dumps(response, indent=4, ensure_ascii=False))
-        raw = response["N1Datas"]
-        for x in raw:
+        RESOURCE_PATH = RESOURCE + QUERY_OPTIONS
+        raw = requests.get(RESOURCE_PATH, headers=auth.get_auth_header()).json()
+        # print(json.dumps(raw, indent=4, ensure_ascii=False))
+        data = raw["N1Datas"]
+
+        for x in data:
             try:
                 aaa = x["SubRouteUID"] + " " + x["StopName"]["Zh_tw"] +  x["SubRouteName"]["Zh_tw"]
                 print(count, aaa)
             except:
                 pass
             count += 1
-        #SubRoute_UID = Route_UID_dict[self.routename] + self.direction
+
+        # SubRoute_UID = Route_UID_dict[self.routename] + self.direction
 
 
 if __name__ == '__main__':
-    auth = Auth(APP_ID, APP_KEY)
+    auth = Authorization.Auth(APP_ID, APP_KEY)
 
     # Fetche the RouteUID from file
-    with open("Routes_info.json", 'r', encoding='utf-8') as outfile:
+    with open("QueryResults/" + "Routes_info.json", 'r', encoding='utf-8') as outfile:
         Route_UID_dict = json.loads(outfile.read())
         outfile.close()
 
